6f46c3fe66d04aa613a1cd51dd52249954651170
Mark Gun <mgun.sd@gmail.com>
2021-03-09 19:15:50 +0000

    Season app initiated with gh-pages pakage

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const App = () => {
+  return (
+    <div>
+      <h1 style={{ textAlign: 'center' }}> this is season app</h1>
+    </div>
+  );
+};
+
+ReactDOM.render(<App />, document.querySelector('#root'));
