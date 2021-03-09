b2c7dc6f621d411c3c257663080ace2d2c1b10ad
Mark Gun <mgun.sd@gmail.com>
2021-03-09 20:09:27 +0000

    State is used in a class comp. & constructor

--- src/index.js
+++ src/index.js
@@ -1,18 +1,33 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 
-const App = () => {
-  navigator.geolocation.getCurrentPosition(
-    //success
-    (konum) => console.log(`enlem: `, konum.coords.latitude),
-    //error
-    (hata) => console.log(`hata Mesaji: `, hata.message)
-  );
-  return (
-    <div>
-      <h1 style={{ textAlign: 'center' }}> this is season app</h1>
-    </div>
-  );
-};
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { konum: null, hata: null };
+
+    navigator.geolocation.getCurrentPosition(
+      //success
+      (konum) => {
+        this.setState({ konum: konum.coords.latitude });
+        /// WRONG!!! state={{ konum: konum.coords.latitude }}
+        console.log(`konum: `, this.state.konum);
+      },
+      //error
+      (hata) => {
+        this.setState({ hata: hata.message });
+        console.log(`hata Mesaji: `, this.state.hata);
+      }
+    );
+  }
+
+  render() {
+    return (
+      <div>
+        <h1 style={{ textAlign: 'center' }}> this is season app</h1>
+      </div>
+    );
+  }
+}
 
 ReactDOM.render(<App />, document.querySelector('#root'));
